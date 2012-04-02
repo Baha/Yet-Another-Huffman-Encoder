f5bd816b7b3f34ca2e15aa0ec3a9ffdbf7266e69
Adrian Palacios <ukonat@gmail.com>
2012-04-03 00:18:20 +0200

    Deleted previous main. Writing codification func.

--- hufftree.cpp
+++ hufftree.cpp
@@ -12,7 +12,8 @@ bool HuffmanLeaf::isLeaf()
 
 void HuffmanTree::buildHuffmanTree(Source source)
 {
-	symbolList(source.getSymbolList());
+	// maybe can cause problems with deletion
+	symbolList = source.getSymbolList();
   rootNode = new HuffmanNode();
 
   rootNode->getCodification(symbolList);
@@ -20,7 +21,40 @@ void HuffmanTree::buildHuffmanTree(Source source)
 
 void HuffmanNode::getCodification(std::list <Symbol> symbolList)
 {
-	std::list <Symbol>::iterator it1 = symbolList.begin();
-	float minProb = 100.0f;
+	std::list <Symbol>::iterator it;
+	std::list <Symbol>::iterator minIt1 = symbolList.begin();
+	float minProb = symbolList.front().getProbability();
+
+	for (it = symbolList.begin(); it != symbolList.end(); it++)
+	{
+		if (minProb > (*it).getProbability())
+		{
+			minProb = (*it).getProbability();
+			minIt1 = it;
+		}
+	}
+
+	// TODO:
+	// extract first min symbol
+	Symbol *symbol1 = &(*minIt1);
+	// delete first min symbol from list
+	symbolList.erase(minIt1);
+
+	// must try until here
+
+	/*
+	minIt1 = symbolList.begin();
+	minProb = symbolList.front().getProbability();
+
+	for (it = symbolList.begin(); it != symbolList.end(); it++)
+	{
+		if (minProb > (*it).getProbability())
+		{
+			minProb = (*it).getProbability();
+			minIt1 = it;
+		}
+	}
+	*/
+
 
 }
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "symbol.h"
-#include "hufftree.h"
-
-int main(int argc, char* argv[])
-{
-  return 0;
-}
