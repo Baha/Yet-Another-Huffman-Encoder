69bdb30ed53db1d61980fae2b7c183395baba6dd
Adrian Palacios <ukonat@gmail.com>
2012-04-12 20:43:23 +0200

    Documentation for the binarizer files added.

--- binarizer.cpp
+++ binarizer.cpp
@@ -1,5 +1,9 @@
 #include "binarizer.h"
 
+/**
+ * The void constructor for the Binarizer.
+ * Resets the attributes from it.
+ */
 Binarizer::Binarizer()
 {
 	binaryCode = "";
@@ -7,11 +11,20 @@ Binarizer::Binarizer()
 	cur_byte = 0;
 }
 
+/**
+ * Returns the number of non-used bits for the
+ * current byte.
+ */
 int Binarizer::getOffset()
 {
 	return (BYTE_LENGTH - bits_used) % BYTE_LENGTH;
 }
 
+/**
+ * This method will be passing bits from the binary
+ * string to a method that will merge them into blocks
+ * in the "binaryCode" string.
+ */
 void Binarizer::addStringToCode(const char* code)
 {
 	int code_length = strlen(code);
@@ -26,6 +39,17 @@ void Binarizer::addStringToCode(const char* code)
 	}
 }
 
+/**
+ * This method calls the "processChar" process in an individual
+ * way for each bit we have to process.
+ *
+ * @param cur_shift The number that indicates which bit of the string we must
+ * process.
+ *
+ * @param bits_to_process The number of bits we have to process.
+ *
+ * @param code A pointer to the binary string we have to codify.
+ */
 void Binarizer::processString(int cur_shift, int bits_to_process, const char* code)
 {
 	for (int i = 0; i < bits_to_process; i++)
@@ -35,6 +59,18 @@ void Binarizer::processString(int cur_shift, int bits_to_process, const char* co
 	}
 }
 
+/**
+ * This method can be tagged as "black magic" if you are not familiar
+ * with the C operators for bit manipulation.
+ *
+ * It just puts the n-th bit of a character (given by "bits_used) to
+ * 1 if the bit is '1'.
+ *
+ * If we have processed 8 bits, we add this character to the binary
+ * string and we reset the counters.
+ *
+ * @param bit The bit to codify into a position of the current byte. 
+ */
 void Binarizer::processChar(char bit)
 {
 	char mask = 0x01;
@@ -44,6 +80,7 @@ void Binarizer::processChar(char bit)
 		mask = mask << bits_used;
 		cur_byte = cur_byte | mask;
 	}
+
 	bits_used++;
 
 	if (bits_used == BYTE_LENGTH)
@@ -54,18 +91,29 @@ void Binarizer::processChar(char bit)
 	}
 }
 
+/**
+ * We add the last byte if we have used at least
+ * 1 bit of it.
+ */
 void Binarizer::finalizeEncoding()
 {
 	if (bits_used != 0)
 		binaryCode.push_back(cur_byte);
 }
 
+/**
+ * We finalize the encoding and return the binary string.
+ */
 std::string Binarizer::printCode()
 {
 	finalizeEncoding();
 	return this->binaryCode;
 }
 
+/**
+ * The void constructor for the Binarizer.
+ * Resets the attributes from it.
+ */
 Debinarizer::Debinarizer()
 {
 	binaryCode = "";
@@ -73,10 +121,20 @@ Debinarizer::Debinarizer()
 	cur_char = 0;
 }
 
+/**
+ * A setter for the "offset" attribute.
+ */
 void Debinarizer::setOffset(int offset)
 {
 	this->offset = offset;
 }
+
+/**
+ * This methods parses every bit from the character
+ * and adds '1' or '0' depending on its bits.
+ *
+ * @param character The character to be parsed.
+ */
 void Debinarizer::addCharToString(char character)
 {
 	for (int i = 0; i < BYTE_LENGTH; i++)
@@ -87,11 +145,17 @@ void Debinarizer::addCharToString(char character)
 	}
 }
 
+/**
+ * Resets the attribute "tempCode".
+ */
 void Debinarizer::resetTempCode()
 {
 	tempCode = "";
 }
 
+/**
+ * Reads a character if there is any left.
+ */
 void Debinarizer::readChar()
 {
 	if (codesLeft())
@@ -101,11 +165,18 @@ void Debinarizer::readChar()
 	}
 }
 
+/**
+ * A getter for the attribute "tempCode".
+ */
 std::string Debinarizer::getTempCode()
 {
 	return this->tempCode;
 }
 
+/**
+ * A method to know if we have characters left
+ * to read.
+ */
 bool Debinarizer::codesLeft()
 {
 	return (cur_char != (binaryCode.size() - offset));
--- binarizer.h
+++ binarizer.h
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * Defines the classes Binarizer and Debinarizer.
+ *
+ * These classes are useful for joining the binary
+ * codes and reading them in a codified file later.
+ */
+
+
 #ifndef _BINARIZER_H_
 #define _BINARIZER_H_
 
@@ -7,6 +16,10 @@
 
 #define BYTE_LENGTH 8
 
+/**
+ * A function for returning the minimum between two
+ * integers.
+ */
 inline int min(int a, int b)
 {
 	if (a < b)
@@ -14,11 +27,32 @@ inline int min(int a, int b)
 	return b;
 }
 
+/**
+ * The Binarizer class is used for building a binary string
+ * that we will write into the codified file.
+ *
+ * We will read the bits from the codes of the characters we
+ * read and we will push them into a string of characters so
+ * we can join more than one codes into one characters.
+ */
 class Binarizer
 {
 protected:
+ /** The "binaryCode" string will contain the string with 
+	 * the new characters that come from the union of the codes
+	 * of the characters we have read from the file.
+	 */
 	std::string binaryCode;
+	/**
+	 * The "bits_used" attribute will count the bits we have
+	 * already used from the byte that we are trying to compose.
+	 */ 
 	unsigned int bits_used;
+	/**
+	 * The "cur_byte" represents a new character that will be
+	 * added to the string once we have put 8 bits of code
+	 * on it.
+	 */
 	char cur_byte;
 public:
 	Binarizer();
@@ -30,12 +64,45 @@ public:
 	std::string printCode();
 };
 
+/**
+ * The Debinarizer class makes the inverse process to binarizing
+ * the code.
+ *
+ * It will read the codified file and will put the bits into a string,
+ * so later we can start reading parts of this string in order to get
+ * the codes and print them into a new decodified file.
+ */ 
 class Debinarizer
 {
 protected:
+  /**
+	 * The "binaryCode" string is a string that will contain only
+	 * 0s or 1s. We will be reading this string for getting the codes
+	 * from the codified file.
+	 *
+	 * Note that a file with N characters to decodify will make this
+	 * string to have a length of 8*N characters, so it can fail while
+	 * analyzing a large file.
+	 */ 
 	std::string binaryCode;
+	/**
+	 * The "tempCode" is an auxiliar string that will contain the bits
+	 * for the code that we are analyzing.
+	 * 
+	 * We will be putting bits into it until we find a code that
+	 * matches the string that "tempCode" contains, so we can emit
+	 * the corresponding character and reset this string.
+	 */
 	std::string tempCode;
+	/**
+	 * "cur_char" is a counter that points the bit to analyze
+	 * in the "binaryCode" string.
+	 */
 	int cur_char;
+	/**
+	 * The "offset" is a number that contains the number of non-used
+	 * bits at the last byte of the codified file.
+	 */
 	int offset;
 public:
 	Debinarizer();
